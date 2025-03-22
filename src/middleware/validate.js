45bdc83d2186a46d623f00b2ace99acceebc1f7b
itsTony4dev <tonyayda7@gmail.com>
2025-03-22 20:13:28 +0200

    send dynamic validation error

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -10,13 +10,13 @@ export const validate = (req, res, next) => {
   const errors = validationResult(req);
   
   if (!errors.isEmpty()) {
-    return res.status(400).json({ 
+    return res.status(400).json({
       success: false,
-      message: 'Validation error', 
-      errors: errors.array().map(err => ({
+      message: errors.array()[0].msg,
+      errors: errors.array().map((err) => ({
         field: err.path,
-        message: err.msg
-      }))
+        message: err.msg,
+      })),
     });
   }
   
