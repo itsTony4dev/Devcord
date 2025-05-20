46a514d57f3b7cb60d4beddc04c96242509b56f0
itsTony4dev <tonyayda7@gmail.com>
2025-05-20 15:00:42 +0300

    add error handling for scraper

--- src/api/scraper/scraper.router.js
+++ src/api/scraper/scraper.router.js
@@ -5,8 +5,12 @@ import { checkCronAuth } from "../../middleware/cronAuth.js";
 const scraperRouter = Router();
 
 scraperRouter.get("/linkedin", checkCronAuth, async (_, res) => {
-  const jobs = await scrapeLinkedInJobs();
-  res.json(jobs);
+  try {
+    const jobs = await scrapeLinkedInJobs();
+    res.json(jobs);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 export default scraperRouter;
